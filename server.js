a47c7d11e7511fd9dc0308606c9a339f3b229654
Ivanchodev <vargasivan203@gmail.com>
2025-04-13 21:14:17 -0400

    Server prube3323

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Esto es una prueba1');
+    res.send('Sesupone que ahora funcione');
 });
 
 // Escucha en todas las interfaces (IP pública incluida)
