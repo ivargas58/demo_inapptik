709ba83f65fb3b7819119d00f2459624f3be1719
Ivanchodev <vargasivan203@gmail.com>
2025-04-13 21:01:31 -0400

    Server prube22

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Esto es una prueba');
+    res.send('Esto es una prueba1');
 });
 
 // Escucha en todas las interfaces (IP pública incluida)
